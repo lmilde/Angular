21390528aebf0c5d701b9133d18b0feae5a443c4
lmilde <lisa.milde@triology.de>
2018-10-25 14:25:03 +0200

    Lektion 97

--- src/app/recipe/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe/recipe-detail/recipe-detail.component.ts
@@ -1,18 +1,21 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
 import {ShoppingListService} from '../../shopping-list/shopping-list.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styles: []
 })
-export class RecipeDetailComponent implements OnInit {
+export class RecipeDetailComponent implements OnInit, OnDestroy {
   selectedRecipe: Recipe;
   recipeId: number;
+
+  private subscription: Subscription;
 
   constructor(private recipeService: RecipeService, private  sls: ShoppingListService, private activatedRoute: ActivatedRoute, private router: Router) {
   }
@@ -31,9 +34,13 @@ export class RecipeDetailComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.activatedRoute.params.subscribe(params => {
+    this.subscription = this.activatedRoute.params.subscribe(params => {
       this.recipeId = +params['id'];
       this.selectedRecipe = this.recipeService.getRecipe(this.recipeId);
     });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
