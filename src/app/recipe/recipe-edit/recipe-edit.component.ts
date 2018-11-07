cb514e66a946c37df2a839e4f1ae081c06324bc5
lmilde <lisa.milde@triology.de>
2018-11-07 14:25:32 +0100

    Lektion 128

--- src/app/recipe/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {RecipeService} from '../recipe.service';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class RecipeEditComponent implements OnInit {
+  recipeForm: FormGroup;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(null, Validators.required),
+      'imagePath': new FormControl(null, Validators.required),
+      'description': new FormControl(null, Validators.required),
+      'ingredients': new FormArray([
+        // new FormGroup({
+        //   'name': new FormControl(null, Validators.required),
+        //   'amount': new FormControl(null, Validators.required)
+        // })
+      ])
+    });
   }
 
 }
